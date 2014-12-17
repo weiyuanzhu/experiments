a7cccefe94559b096b7ea83358300c3f0e628a23
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-12-17 11:48:09 +0000

    add wifi p2p text server

--- wifi_direct_server/src/main/java/com/mackwell/wifi_direct_server/WifiP2PServer.java
+++ wifi_direct_server/src/main/java/com/mackwell/wifi_direct_server/WifiP2PServer.java
@@ -1,10 +1,9 @@
 package com.mackwell.wifi_direct_server;
 
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,21 +11,31 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class WifiP2PServer extends ActionBarActivity {
+public class WifiP2PServer extends Activity {
+
+    static final String TAG = "WIFI_direct_server";
+
+    private TextView statusTextView;
+    private TextView messageTextView;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_p2p_server);
+
+        statusTextView = (TextView) findViewById(R.id.textView);
+        messageTextView = (TextView) findViewById(R.id.textView2);
+        mHandler = new Handler();
     }
 
 
@@ -61,26 +70,86 @@ public class WifiP2PServer extends ActionBarActivity {
     Runnable test = new Runnable() {
         @Override
         public void run() {
+            Socket clientSocket = null;
 
             byte[] buffer = new byte[100];
             String inputLine;
 
             try {
                 ServerSocket serverSocket = new ServerSocket(8888);
-                Socket client = serverSocket.accept();
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(client.getInputStream()));
+                mHandler.post(new UpdateStatus("Server started listening"));
 
-                while ((inputLine = in.readLine()) != null) {
+                while(true) {
+                    mHandler.post(new UpdateStatus("Server waits for connection"));
+                    clientSocket = serverSocket.accept();
+                    mHandler.post(new UpdateStatus("Connection accepted" + clientSocket.getInetAddress() + ": " + clientSocket.getPort()));
+
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+
+                    while (true) {
+                        if ((inputLine = in.readLine()) != null) {
+                            Log.d(TAG, "Text received: " + inputLine);
+                            mHandler.post(new UpdateMessage(inputLine));
+                            if(inputLine.equals("exit")){
+                                mHandler.post(new UpdateStatus("Server waits for connection"));
+                                break;
+                            }
+                        }
+                    }
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
+
+                try {
+                    if(clientSocket!=null) clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
         }
     };
+
+    public void clear(View view){
+        messageTextView.setText("Received message:");
+
+    }
+
+
+    class UpdateStatus implements Runnable{
+
+        private String status;
+
+        public UpdateStatus(String status){
+            this.status = status;
+        }
+        @Override
+        public void run() {
+            statusTextView.setText(status);
+        }
+    }
+
+    class UpdateMessage implements Runnable{
+
+        private String message;
+
+        public UpdateMessage(String status){
+            this.message = status;
+        }
+        @Override
+        public void run() {
+            SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm:ss");
+
+            Date resultDate = new Date(System.currentTimeMillis());
+
+
+            messageTextView.append("\n"+ sdf.format(resultDate) +":     " +  message);
+        }
+    }
 
 
 
